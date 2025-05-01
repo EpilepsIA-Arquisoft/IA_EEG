51934c98bec69c4f6a46772f1258ddbb94859af4
DavidCepeda13 <j.cepedab@uniandes.edu.co>
2025-05-01 10:47:12 -0500

    fixed_forever

--- run_forever.sh
+++ run_forever.sh
@@ -1,6 +1,6 @@
 while true; do
     echo "Iniciando script..."
-    python3 /IA/Start.py
+    python3 IA/Start.py
     echo "Script terminado. Esperando 10 segundos antes de reiniciar..."
     sleep 10
 done
